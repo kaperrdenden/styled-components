445764c2ca7df7fe71e03ccf46b21db283856fa4
kaperrdenden <weather2sogood@gmail.com>
2022-02-22 23:53:07 +0900

    styled-component내부에서 props를 받을때에도 비구조화할당 문법 사용 가능

--- src/components/Button.js
+++ src/components/Button.js
@@ -20,8 +20,8 @@ const StyledButton = styled.button`
     font-size: 1rem;
 
     // 색상
-    ${props => {
-        const selected = props.theme.palette[props.color];
+    ${({ theme, color}) => {
+        const selected = theme.palette[color];
         return css`
             background: ${selected};
             &:hover{
