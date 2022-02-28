d8db8ff6f26624e34c728b47970b49b408a258c1
kaperrdenden <96989013+kaperrdenden@users.noreply.github.com>
2022-02-28 23:35:11 +0900

    Update Button.js

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,6 +2,7 @@ import React from 'react';
 import styled, {css} from 'styled-components';
 import {darken, lighten} from 'polished';
 
+
 const colorStyles = css`
     ${({ theme, color })=>{
         const selected = theme.palette[color];
